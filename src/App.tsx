2d5b4380cf66de41ce909413791e8eed53462867
FatinYazim <fatin_yazim@yahoo.com>
2023-06-22 16:56:28 +0800

    test

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Definition from "./pages/Definition";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+        
+      
+    },
+    {
+      path: "/:word",
+      element: <Definition/>,
+    },
+  ]);
+
+  return  <RouterProvider router={router} />
+}
+
+export default App;
